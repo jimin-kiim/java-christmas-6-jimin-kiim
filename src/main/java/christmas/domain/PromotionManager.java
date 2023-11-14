a04eb56326beda644cadb1d0a5f606cc79ac747a
Jimin Kim <0305jimin@gmail.com>
2023-11-15 06:49:24 +0900

    Fix: 증정 이벤트 할인 금액 0으로 수정

--- src/main/java/christmas/domain/PromotionManager.java
+++ src/main/java/christmas/domain/PromotionManager.java
@@ -16,7 +16,7 @@ public class PromotionManager {
     public void applyGiftEvent(Reservation reservation) {
         int prePromotionTotal = reservation.getPrePromotionTotal();
         if (prePromotionTotal >= 120000) {
-            reservation.addAppliedPromotionList(new Promotion("증정 이벤트", 25000));
+            reservation.addAppliedPromotionList(new Promotion("증정 이벤트", 0));
             reservation.setGift(new OrderItem(Menu.CHAMPAGNE.getName(), 1));
         }
     }
